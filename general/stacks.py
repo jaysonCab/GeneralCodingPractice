8c875e73b7257102bfca6a9baf1e6247ab711d17
shinobu <shinobu@kocho.localdomain>
2024-12-08 10:34:03 -0500

    Stack and Queue notes

--- general/stacks.py
+++ general/stacks.py
@@ -31,5 +31,55 @@ FILO: Append and Pop with no pop parameter, will remove the most recent entry or
 
 # QUEUE -----------------------------------------------------------------
 '''
-
+A queue is basically the same as a stack but instead of leaving the pop() blank, you pass in the parameter 0
+The problem when you pop from the front, is that every item in the list needs to move up one spot so it is O(n)
+To fix this, try and from collections import deque
+    Regular library is called collections
 '''
+
+
+    # Simple append and pop(0) in a queue
+# stackList = []
+# stackList.append(5)
+# stackList.append(10)
+# stackList.append(15)
+# print(stackList)
+
+# element = stackList.pop(0)
+# print(f'The stackList is {stackList} and the popped element is {element}')
+
+
+    # Avoids having to move the entire list if something is popped at position 0
+# from collections import deque
+# data = deque('123512416243')
+
+# data.append('5')
+# # data.popleft
+
+# print(list(data))
+
+
+# CUSTOM STACK AND QUEUE CLASSES -----------------------------------------------------------------
+'''
+Simple class and object creation
+Placing a variable with an underscore means it is meant to be private
+'''
+
+# class Stack:
+#     def __init__(self):
+#         self._data = []
+
+#     def push(self, data):
+#         self._data.append(data)
+#         print(self._data)
+
+#     def pop(self):
+#         return self._data.pop()
+    
+# stack1 = Stack()
+# stack1.push(10)
+# stack1.push(20)
+# stack1.push(30)
+# element = stack1.pop()
+
+# print(element)
